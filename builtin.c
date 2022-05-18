bb60f1f72b91e1f22715d190fdc0c6d4eee56835
SharonMutabonwa <mutaron37@gmail.com>
2022-05-18 19:22:54 +0000

    Creating a simple shell

--- builtin.c
+++ builtin.c
@@ -1,119 +1,36 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * _unsetenv - removes an environment variable
- * @name: name of the environment variable
- * Return: 0 on sucess otheewise -1
+ * exit_shell - This will run the builtin exit
+ * @line: Line buffer of user input
+ * @args: Arguments from user
+ * @env: Environment
+ * Return: Void
  */
-
-int _unsetenv(char *name)
+void exit_shell(char **args, char *line, char **env)
 {
-	char **new_env;
-	unsigned int i = 0, count = 0;
-	int cmp_val;
+	free(args);
+	free(line);
+	(void)env;
+	exit(0);
+}
+/**
+ * env_shell - Prints shell environment
+ * @args: Arguments split from cmdline
+ * @line: User input buffer
+ * @env: Environment
+ */
+void env_shell(char **args, char *line, char **env)
+{
+	int size, i = 0;
 
-	while (environ[i])
+	while (env[i] != NULL)
 	{
-		cmp_val = _strcmp(name, environ[i]);
-		if (cmp_val == 0)
-			count++;
+		size = _strlen(env[i]);
+		write(1, env[i], size);
+		write(1, "\n", 1);
 		i++;
 	}
-	i--;
-	if (count == 0)
-		return (0);
-	new_env = malloc(i * sizeof(char *));
-	if (new_env == NULL)
-	{
-		perror("Unable to remove env variable");
-		return (-1);
-	}
-	count = 0;
-	for (i = 0; environ[i]; i++)
-	{
-		cmp_val =  _strcmp(name, environ[i]);
-		if (cmp_val == 0)
-			;
-		else
-			new_env[count++] = environ[i];
-	}
-	new_env[count++] = NULL;
-	environ = new_env;
-	return (0);
-}
-
-/**
- * _setenv -  set an environmental variable
- * @name: name of the variable
- * @value: value of the variable
- * Return: 0 on success and -1 on failure
- */
-
-int _setenv(char *name, char *value)
-{
-	int i = 0, count = 0;
-	char **new_env = NULL, *new_var = NULL;
-
-	while (environ[i] != NULL)
-	{
-		if (_strcmp(name, environ[i]) == 0)
-			count++;
-		i++;
-	}
-	printf("count : %d\n", count);
-	if (count != 0)
-		new_env = malloc(sizeof(char *) * i);
-	else
-		new_env = malloc(sizeof(char *) * (i + 1));
-	count = 0;
-	new_var = setenv_cat(name, value);
-	if (new_var == NULL)
-	{
-		free(new_env);
-		perror("unable to set environmental variable");
-		return (-1);
-	}
-	for (i = 0; environ[i]; i++)
-	{
-		if (_strcmp(name, environ[i]) == 0)
-		{
-			new_env[i] = new_var;
-			count++;
-		}
-		else
-			new_env[i] = environ[i];
-	}
-	if (count == 0)
-		new_env[i++] = new_var;
-	new_env[i] = NULL;
-	environ = new_env;
-	return (0);
-}
-
-/**
- * setenv_cat - concats name and value about to be set
- * @name: name of env variable
- * @value: value of env variable
- * Return: a pointer to the concatenated env variable
- */
-
-char *setenv_cat(char *name, char *value)
-{
-	int len, i;
-	char *new_var;
-
-	len = _strlen(name) + _strlen(value) + 2;
-	if (len <= 2)
-		return (NULL);
-	new_var = malloc(sizeof(char) * len);
-	if (new_var == NULL)
-		return (NULL);
-	for (len = 0; name[len]; len++)
-		new_var[len] = name[len];
-	new_var[len] = '=';
-	len++;
-	for (i = 0; value[i]; i++, len++)
-		new_var[len] = value[i];
-	new_var[len] = '\0';
-	return (new_var);
+	(void)args;
+	(void)line;
 }
--- main.c
+++ main.c
@@ -1,36 +1,20 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * main - Entry point
- * @argc: number of arguements
- * @argv: arguement passed into the function
- * Description: Standard output display of shell.
- * Return: 0 on success.
+ * main - Entry point for shell, handles args to shell
+ * @ac: Arg count
+ * @av: Arr of args
+ * @env: Environment
+ * Return: 0;
  */
-
-int main(__attribute__((unused))int argc, char **argv)
+int main(int ac, char **av, char **env)
 {
-	char *line = NULL, **args;
-	int status = 1;
-	char **chk_env = environ;
-	size_t n = 0;
-	unsigned int count;
-
-	do {
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "($) ", 4);
-		if (chk_env != environ)
-			chk_env = NULL;
-		count = _getline(&line, &n, stdin);
-		if (count == 1)
-			continue;
-		args = get_args(line);
-		status = builtin_args(args);
-		if (status < 0)
-			status = execute_cmd(args, argv);
-		free(args);
-	} while (status);
-	if (chk_env == NULL)
-		free(environ);
+	if (!ac)
+		(void)ac;
+	if (!av)
+		(void)av;
+	if (!env)
+		(void)env;
+	shell(ac, av, env);
 	return (0);
 }
