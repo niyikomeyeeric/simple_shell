05431e87d3693b09219a78c4e9db69c5a8b5c4ce
SharonMutabonwa <mutaron37@gmail.com>
2022-05-19 10:56:12 +0000

    fixed errors

--- main.c
+++ main.c
@@ -1,38 +1,36 @@
 #include "main.h"
 
 /**
- * main - entry point for application
- * @ac: argument count
- * @av: argument vector
- * Return: 0 on success
+ * main - Entry point
+ * @argc: number of arguements
+ * @argv: arguement passed into the function
+ * Description: Standard output display of shell.
+ * Return: 0 on success.
  */
-int main(int ac, char **av)
-{
-	config build;
 
-	(void)ac;
-	signal(SIGINT, sigintHandler);
-	configInit(&build);
-	build.shellName = av[0];
-	shell(&build);
+int main(__attribute__((unused))int argc, char **argv)
+{
+	char *line = NULL, **args;
+	int status = 1;
+	char **chk_env = environ;
+	size_t n = 0;
+	unsigned int count;
+
+	do {
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "($) ", 4);
+		if (chk_env != environ)
+			chk_env = NULL;
+		count = _getline(&line, &n, stdin);
+		if (count == 1)
+			continue;
+		args = get_args(line);
+		status = builtin_args(args, argv);
+		if (status < 0)
+			status = execute_cmd(args, argv);
+		free(args);
+	} while (status);
+	if (chk_env == NULL)
+		free(environ);
 	return (0);
-}
-
-/**
- * configInit - initialize member values for config struct
- * @build: input build
- * Return: build with initialized members
- */
-config *configInit(config *build)
-{
-	build->env = generateLinkedList(environ);
-	build->envList = NULL;
-	build->args = NULL;
-	build->buffer = NULL;
-	build->path = _getenv("PATH", environ);
-	build->fullPath = NULL;
-	build->lineCounter = 0;
-	build->shellName = NULL;
-	build->errorStatus = 0;
-	return (build);
 }
--- main.h
+++ main.h
@@ -1,192 +1,58 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <sys/wait.h>
-#include <unistd.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <signal.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <limits.h>
+#define BUFFSIZE 1024
 
-/* ERRORS */
-#define BUFSIZE 256
-#define ENOSTRING 1106
-#define EILLEGAL 227
-#define EWSIZE 410
-#define ENOBUILTIN 415
-#define EBADCD 726
-#define true 1
-#define false 0
-
+/**
+ * struct builtin_fun - struct that execute builtin funnction if seen
+ * @name: string to be compared with
+ * @f: function pointer
+ * Description: struct for builtin function
+ */
+typedef struct builtin_fun
+{
+	char *name;
+	void (*f)(char **args, char **argv);
+} built_in;
 extern char **environ;
+void sh_loop(void);
+char *eval_str(char *str, char **nxt_ptr, const char *delim);
+char *check_str(char *str, const char *delim);
+char *_strtok(char *str, const char *delim);
 
-/* boolean type - true as 1 and false as 0*/
-typedef int bool;
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+char *_getenv(char *name);
+int _unsetenv(char *name);
+char *setenv_cat(char *name, char *value);
+int _setenv(char *name, char *value);
+char **get_args(char *line);
 
-/**
- * struct linkedList - linked list data structure
- * @string: environ variable path name
- * @next: pointer to next node
- */
-typedef struct linkedList
-{
-	char *string;
-	struct linkedList *next;
-} linked_l;
-
-/**
- * struct configurations - configuration of build settings
- * @env: linked list of local env variables
- * @envList: array of env variables to put into execve
- * @args: array of argument strings
- * @buffer: string buffer of user input
- * @path: array of $PATH locations
- * @fullPath: string of path with correct prepended $PATH
- * @shellName: name of shell (argv[0])
- * @lineCounter: counter of lines users have entered
- * @errorStatus: error status of last child process
- */
-typedef struct configurations
-{
-	linked_l *env;
-	char **envList;
-	char **args;
-	char *buffer;
-	char *path;
-	char *fullPath;
-	char *shellName;
-	unsigned int lineCounter;
-	int errorStatus;
-} config;
-
-/**
- * struct builtInCommands - commands and functions associated with it
- * @command: input command
- * @func: output function
- */
-typedef struct builtInCommands
-{
-	char *command;
-	int (*func)(config *build);
-} type_b;
-
-/* main */
-config *configInit(config *build);
-
-/* built_ins */
-bool findBuiltIns(config *build);
-int exitFunc(config *build);
-int historyFunc(config *build);
-int aliasFunc(config *build);
-
-/* cd */
-int cdFunc(config *);
-bool cdToHome(config *build);
-bool cdToPrevious(config *build);
-bool cdToCustom(config *build);
-bool updateEnviron(config *build);
-
-/* cd2 */
-int updateOld(config *build);
-bool updateCur(config *build, int index);
-
-/* env */
-int envFunc(config *build);
-int setenvFunc(config *build);
-int unsetenvFunc(config *build);
-int _isalpha(int c);
-
-/* help */
-int helpFunc(config *build);
-int displayHelpMenu(void);
-int helpExit(config *build);
-int helpEnv(config *build);
-int helpHistory(config *build);
-
-/* help2 */
-int helpAlias(config *build);
-int helpCd(config *biuld);
-int helpSetenv(config *build);
-int helpUnsetenv(config *build);
-int helpHelp(config *build);
-
-/* built_in_helpers*/
-int countArgs(char **args);
-int _atoi(char *s);
-
-/* shell */
-void shell(config *build);
-void checkAndGetLine(config *build);
-void forkAndExecute(config *build);
-void stripComments(char *str);
-void convertLLtoArr(config *build);
-
-/* _getenv */
-char *_getenv(char *input, char **environ);
-
-/* error_handler */
-void errorHandler(config *build);
-unsigned int countDigits(int num);
-char *itoa(unsigned int num);
-char *getErrorMessage();
-
-/* shell_helpers */
-void insertNullByte(char *str, unsigned int index);
-void displayPrompt(void);
-void displayNewLine(void);
-void sigintHandler(int sigint);
-
-/* check_path */
-bool checkPath(config *);
-bool checkEdgeCases(config *build);
-
-/* split_string */
-bool splitString(config *build);
-unsigned int countWords(char *s);
-bool isSpace(char c);
-
-/* string_helpers1 */
 int _strlen(char *s);
-char *_strcat(char *dest, char *src);
-int _strcmp(char *s1, char *s2);
+char *_strcat(char *src, char *dest);
 char *_strdup(char *str);
-char *_strcpy(char *dest, char *src);
+int _strcmp(char *strcmp1, char *strcmp2);
+int _atoi(char *str);
 
-/* string_helpers2 */
-char *_strtok(char *str, char *delim);
-int _strcspn(char *string, char *chars);
-char *_strchr(char *s, char c);
-
-/* llfuncs1 */
-linked_l *addNode(linked_l **head, char *str);
-linked_l *addNodeEnd(linked_l **head, char *str);
-size_t printList(const linked_l *h);
-int searchNode(linked_l *head, char *str);
-size_t list_len(linked_l *h);
-
-/* llfuncs2 */
-int deleteNodeAtIndex(linked_l **head, unsigned int index);
-linked_l *generateLinkedList(char **array);
-linked_l *addNodeAtIndex(linked_l **head, int index, char *str);
-char *getNodeAtIndex(linked_l *head, unsigned int index);
-
-/* welcome */
-void welcome_screen_1(void);
-void welcome_screen_2(void);
-
-/* _realloc */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_memcpy(char *dest, char *src, unsigned int n);
-
-/* free */
-void freeMembers(config *build);
-void freeArgsAndBuffer(config *build);
-void freeArgs(char **args);
-void freeList(linked_l *head);
+int builtin_args(char **args, char **argv);
+void bui_exit(char **args, char **argv);
+void bui_env(char **args, char **argv);
+void bui_unsetenv(__attribute__((unused))char **args, char **argv);
+void bui_setenv(char **args, char **argv);
+int execute_cmd(char **args, char **argv);
+int check_execute(char **args, char **argv);
+int check_dir(char **args);
+void execute(char **args, char **argv);
+int exec_cmd(char **args, char **argv);
+int check_cwd(int check_run, char **args, char **argv);
+void check_cmd_tty(char **args, char **argv);
 
 #endif
